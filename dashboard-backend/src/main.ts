50b77ddeda97d20bc6718adc8fc106dcaf095560
Tim Brown <timbrownls26@hotmail.com>
2025-11-06 07:31:41 +1000

    fix

--- dashboard-backend/src/main.ts
+++ dashboard-backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as session from 'express-session';
+import session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
